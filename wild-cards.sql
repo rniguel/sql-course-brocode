faedbcb537fe300f3a83b13e088eed7ee7960551
Miguel Riquelme <miguelrisquelme@gmail.com>
2026-04-05 20:17:15 -0300

    feat: add LIKE wildcard pattern matching examples
    - % and _ wildcards for first_name, last_name, hire_date, job

---
+++ wild-cards.sql
@@ -0,0 +1,17 @@
+SELECT *
+FROM employees;
+SELECT *
+FROM employees
+WHERE first_name LIKE "A%";
+SELECT *
+FROM employees
+WHERE hire_date LIKE "2020%";
+SELECT *
+FROM employees
+WHERE last_name LIKE "%n";
+SELECT *
+FROM employees
+WHERE first_name LIKE "A%";
+SELECT *
+FROM employees
+WHERE job LIKE "_a%";
