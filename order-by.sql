ec3d636adb1c4bbbfc954b0837f0f795c0755df0
Miguel Riquelme <miguelrisquelme@gmail.com>
2026-04-05 20:17:19 -0300

    feat: add ORDER BY sorting examples
    - ASC, DESC, and multi-column sorting

---
+++ order-by.sql
@@ -0,0 +1,12 @@
+SELECT *
+FROM employees;
+SELECT *
+FROM employees
+ORDER BY last_name;
+SELECT *
+FROM employees
+ORDER BY hire_date DESC;
+SELECT *
+FROM transactions
+ORDER BY amount,
+  customer_id;
